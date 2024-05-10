c62914c602cc6ae2896190609b4a323e489bbc24
Mila Tran <mila.tran@contemi.com>
2024-05-10 14:52:29 +0700

    multiple states in 1 variable object

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -4,23 +4,32 @@ import "./GameStyle.css"
 import {calculateWinner} from "../../helpers";
 
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(board);
+    // const [board, setBoard] = useState(Array(9).fill(null));
+    // const [xIsNext, setXIsNext] = useState(true);
+    const [state, setState] = useState({
+        board: Array(9).fill(null),
+        xIsNext: true,
+    })
+    const winner = calculateWinner(state.board);
     const handleClick = (index) => {
-        const boardCopy = [...board]; // tao ra 1 ban clone cua board
+        const boardCopy = [...state.board]; // tao ra 1 ban clone cua board
         if(winner || boardCopy[index]) return;
-        boardCopy[index] = xIsNext ? 'X' : 'O';
-        setBoard(boardCopy);
-        setXIsNext(!xIsNext);
+        boardCopy[index] = state.xIsNext ? 'X' : 'O';
+        // setBoard(boardCopy);
+        // setXIsNext(!state.xIsNext);
+        setState({
+            ...state, // tao ra 1 ban clone cua state truoc khi update state de ko mat cac item ben trong state
+            board: boardCopy,
+            xIsNext: !state.xIsNext,
+        })
     };
     const handleResetGame = () => {
-        setBoard(Array(9).fill(null));
-        setXIsNext(true);
+        // setBoard(Array(9).fill(null));
+        // setXIsNext(true);
     };
     return (
         <div>
-            <Board cells={board} onClick={handleClick}></Board>
+            <Board cells={state.board} onClick={handleClick}></Board>
             <button className="btn-primary" onClick={handleResetGame}>Reset game</button>
             {winner && <h1 className="game-winner">
                 {winner ? `Winner is ${winner}` : ""}
